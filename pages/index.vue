2d566a5975252808a154a2dad1ddcfd3587a07b2
Welliton Klein <welliton.fokushima@gmail.com>
2019-05-03 18:10:01 -0300

    Adicionar Login/Register, criar Navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="md-layout md-alignment-center">
+  <div class="md-layout md-alignment-center" style="margin: 4em 0">
+    <!-- Top Navigation -->
+    <md-toolbar class="fixed-toolbar" elevation="1">
+      <md-button class="md-icon-button">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <nuxt-link class="md-primary md-title" to="/">NuxtNews</nuxt-link>
+
+      <div class="md-toolbar-section-end">
+        <md-button @click="$router.push('/login')">Login</md-button>
+        <md-button @click="$router.push('/register')">Register</md-button>
+      </div>
+    </md-toolbar>
+
+    <!-- App Content -->
     <div class="md-layout-item md-size-95">
       <md-content class="md-layout md-gutter" style="background: #007998; padding: 1em;">
         <ul 
@@ -63,5 +77,11 @@ export default {
 <style scoped>
   .small-icon {
     font-size: 18px !important;
+  }
+
+  .fixed-toolbar {
+    position: fixed;
+    top: 0;
+    z-index: 5;
   }
 </style>
