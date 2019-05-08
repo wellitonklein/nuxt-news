191e8bf1e182cd8bd1ca1e52e68bd08e4b1b7c08
Welliton Klein <welliton.fokushima@gmail.com>
2019-05-08 12:29:19 -0300

    Query Headlines por termos de pesquisa, data e critérios

--- pages/index.vue
+++ pages/index.vue
@@ -21,9 +21,47 @@
           <md-button @click="$router.push('/login')">Login</md-button>
           <md-button @click="$router.push('/register')">Register</md-button>
         </template>
+
+        <md-button class="md-primary" @click="showSearchDialog = true">Search</md-button>
+
         <md-button class="md-accent" @click="showRightSidepanel = true">Categories</md-button>
       </div>
     </md-toolbar>
+
+    <!-- Search Dialog -->
+    <md-dialog :md-active.sync="showSearchDialog">
+      <md-dialog-title>Search Headlines</md-dialog-title>
+
+      <div class="md-layout" style="padding: 1em">
+        <md-field>
+          <label>Searhc Term(s)</label>
+          <md-input
+            v-model="query"
+            placeholder="Use quotes for exact matches, AND / OR / NOT for multiple terms"
+            maxlength="30"
+          ></md-input>
+        </md-field>
+        <md-datepicker v-model="fromDate" md-immediately>
+          <label>Select starting date (optional)</label>
+        </md-datepicker>
+        <md-datepicker v-model="toDate" md-immediately>
+          <label>Select ending date (optional)</label>
+        </md-datepicker>
+        <md-field>
+          <label for="sortBy">Sort search results by criteria (optional)</label>
+          <md-select v-model="sortBy" name="sortBy" id="sortBy" md-dense>
+            <md-option value="publishedAt">Newest (default)</md-option>
+            <md-option value="relevancy">Relevancy</md-option>
+            <md-option value="popularity">Popular</md-option>
+          </md-select>
+        </md-field>
+      </div>
+
+      <md-dialog-actions>
+        <md-button class="md-accent" @click="showSearchDialog = false">Cancel</md-button>
+        <md-button class="md-primary" @click="searchHeadlines">Search</md-button>
+      </md-dialog-actions>
+    </md-dialog>
 
     <!-- Personal News Feed (Left Drawer) -->
     <md-drawer md-fixed :md-active.sync="showLeftSidepanel">
@@ -171,6 +209,7 @@ export default {
   data: () => ({
     showLeftSidepanel: false,
     showRightSidepanel: false,
+    showSearchDialog: false,
     newsCategories: [
       { name: "Top Headlines", path: "", icon: "today" },
       { name: "Technology", path: "technology", icon: "keyboard" },
@@ -179,7 +218,11 @@ export default {
       { name: "Health", path: "health", icon: "fastfood" },
       { name: "Science", path: "science", icon: "fingerprint" },
       { name: "Sports", path: "sports", icon: "golf_course" }
-    ]
+    ],
+    query: "",
+    fromDate: "",
+    toDate: "",
+    sortBy: ""
   }),
   async fetch({ store }) {
     await store.dispatch(
@@ -233,11 +276,23 @@ export default {
       );
       this.showRightSidepanel = false;
     },
-    async loadSource (sourceId) {
+    async loadSource(sourceId) {
       if (sourceId) {
-        this.$store.commit('setSource', sourceId)
-        await this.$store.dispatch('loadHeadlines', `/api/top-headlines?sources=${this.source}`)
+        this.$store.commit("setSource", sourceId);
+        await this.$store.dispatch(
+          "loadHeadlines",
+          `/api/top-headlines?sources=${this.source}`
+        );
       }
+    },
+    async searchHeadlines() {
+      await this.$store.dispatch(
+        "loadHeadlines",
+        `/api/everything?q=${this.query}&from=${this.dateToISOString(
+          this.fromDate
+        )}&to=${this.dateToISOString(this.toDate)}&sortBy=${this.sortBy}`
+      );
+      this.showSearchDialog = false;
     },
     async addHeadlineToFeed(headline) {
       if (this.user) {
@@ -248,9 +303,12 @@ export default {
       await this.$store.dispatch("removeHeadlineFromFeed", headline);
     },
     async saveHeadline(headline) {
-      await this.$store.dispatch('saveHeadline', headline).then(() => {
-        this.$router.push(`/headlines/${headline.slug}`)
-      }).catch(err => console.error(err))
+      await this.$store
+        .dispatch("saveHeadline", headline)
+        .then(() => {
+          this.$router.push(`/headlines/${headline.slug}`);
+        })
+        .catch(err => console.error(err));
     },
     changeCountry(country) {
       this.$store.commit("setCountry", country);
@@ -262,6 +320,11 @@ export default {
       const inFeed =
         this.feed.findIndex(headline => headline.title === title) > -1;
       return inFeed ? "md-primary" : "";
+    },
+    dateToISOString(date) {
+      if (date) {
+        return new Date(date).toISOString();
+      }
     }
   }
 };
