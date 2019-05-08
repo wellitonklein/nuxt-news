ab0de48c6b42fa9dd01c756317df4d9b6dce2fe9
Welliton Klein <welliton.fokushima@gmail.com>
2019-05-08 13:04:29 -0300

    Add default image to Headlines without Media

--- pages/headlines/_slug.vue
+++ pages/headlines/_slug.vue
@@ -74,7 +74,7 @@
       <!-- Back Button -->
       <md-button
         class="md-fixed md-fab-bottom-right md-fab md-primary"
-        @click="$router.go(-1)"commentTimeToNow
+        @click="$router.go(-1)"
       >
         <md-icon>arrow_back</md-icon>
       </md-button>
